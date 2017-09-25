6943b8f4dc9e06fe4f23ecdc784482dc560809e2
Long Tran <lngtrn94@gmail.com>
2017-09-25 15:06:58 +0200

    count message types

--- src/main/java/in/dragonbra/MessageTypeStats.java
+++ src/main/java/in/dragonbra/MessageTypeStats.java
@@ -9,27 +9,25 @@ import org.apache.spark.sql.SparkSession;
 import org.opensky.libadsb.Decoder;
 import org.opensky.libadsb.msgs.ModeSReply;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-public class FlightVisualizer {
-
-    public static final String OUTPUT_PATH = "sample-out";
+public class MessageTypeStats {
 
     public static void main(String[] args) throws IOException {
 
-        if (args.length == 0) {
-            System.out.println("Usage: [input directory]");
+        if (args.length < 2) {
+            System.out.println("Usage: [input directory] [output file]");
             System.exit(1);
         }
 
+        long start = System.currentTimeMillis();
+
         Collection<File> files = FileUtils.listFiles(new File(args[0]), new String[]{"avro"}, true);
         List<String> paths = files.stream().map(File::getAbsolutePath).collect(Collectors.toList());
-
-        FileUtils.deleteDirectory(new File(OUTPUT_PATH));
 
         SparkSession spark = SparkSession
                 .builder()
@@ -41,22 +39,28 @@ public class FlightVisualizer {
         Dataset<Row> df = spark.read().format("com.databricks.spark.avro")
                 .load(paths.toArray(new String[paths.size()]));
 
-        JavaRDD<ModeSReply> messages = df.sort("timeAtServer")
-                .select("timeAtServer", "rawMessage")
+        JavaRDD<ModeSReply.subtype> types = df.select("rawMessage")
                 .map(value -> {
                     try {
-                        return Decoder.genericDecoder(value.getString(1));
+                        return Decoder.genericDecoder(value.getString(0)).getType();
                     } catch (Exception ignored) {
                     }
-                    return null;
-                }, Encoders.kryo(ModeSReply.class))
-                .filter(p -> p != null
-                        && (p.getType() == ModeSReply.subtype.ADSB_AIRBORN_POSITION
-                        || p.getType() == ModeSReply.subtype.ADSB_SURFACE_POSITION))
+                    return ModeSReply.subtype.MODES_REPLY;
+                }, Encoders.kryo(ModeSReply.subtype.class))
                 .javaRDD();
 
-        messages.saveAsTextFile(OUTPUT_PATH);
+        Map<ModeSReply.subtype, Long> typeCount = types.countByValue();
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(args[1]), "utf-8"))) {
+            for (Map.Entry<ModeSReply.subtype, Long> entry : typeCount.entrySet()) {
+                writer.write(entry.getKey().toString() + "," + entry.getValue().toString());
+                writer.write('\n');
+            }
+        }
 
         spark.stop();
+
+        System.out.println("Finished in " + (System.currentTimeMillis() - start) + " milliseconds.");
     }
 }
